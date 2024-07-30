fbf66a9a665660495410a68fdd21517d1724563d
Percival-Ashley <percivalashley1@gmail.com>
2024-07-31 00:57:21 +0300

    Updated index.js
    Moved  the city element from handleSearchSubmit  function to refreshWeather function
    
    This will unable the API results name to be used even if one mistypes e.g use caps in between

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = searchInput.value;
 }
 function searchCity(city) {
   let apiKey = "3471d6bbaf0b6da9tcfad5o5ae1dc50c";
@@ -11,9 +14,7 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
 
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
