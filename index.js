d1c59db0874e4e78df487f6a17fdbf6fb0bb745a
Percival-Ashley <percivalashley1@gmail.com>
2024-07-31 00:31:43 +0300

    Updated index.js
    Created new function to receive the response from this API results, which will basically have info of the city we are searching for
    
    To do so, I used axois.get(apiUrl)l.then(refreshWeather) to call it

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+function refreshWeather(response) {}
 function searchCity(city) {
   let apiKey = "3471d6bbaf0b6da9tcfad5o5ae1dc50c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 function handleSearchSubmit(event) {
   event.preventDefault();
