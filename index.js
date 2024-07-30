2502e31e58d6502c899c7ffd7cc3006980d92ef8
Percival-Ashley <percivalashley1@gmail.com>
2024-07-31 00:16:25 +0300

    Making an API call
    Added my API key
    Added API URL
    Made query=city and key=apiUrl

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function searchCity(city) {}
+function searchCity(city) {
+  let apiKey = "3471d6bbaf0b6da9tcfad5o5ae1dc50c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
