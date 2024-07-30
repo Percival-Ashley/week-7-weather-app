303dc0100b046f07d73793b6b297d14d8d9380b2
Percival-Ashley <percivalashley1@gmail.com>
2024-07-31 00:07:19 +0300

    Updated index.js
    Created second function to receive city name and update UI

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+function searchCity(city) {}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
 
   cityElement.innerHTML = searchInput.value;
-  console.log(searchInput.value);
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
