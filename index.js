7ea63818bb807a524f80a30367668b98b1a771b4
Percival-Ashley <percivalashley1@gmail.com>
2024-10-02 13:51:50 +0300

    Displayed 5 days instead of 7
    Added index and if statement to do so

--- index.js
+++ index.js
@@ -58,10 +58,11 @@ function getForecast(city) {
 function displayForecast(response) {
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
                 <div class="weather-forecast-date">Tue</div>
                 <img src="${
                   day.condition.icon_url
@@ -76,6 +77,7 @@ function displayForecast(response) {
             </div>
             </div>
             `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
